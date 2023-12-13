45044df856f0f621f6fd9bd05000f5cfe77bba13
Kai Wright <wright.kai@outlook.com>
2023-12-14 09:40:10 +1000

    Stellar : Improved background and better CSS Reset

--- Stellar/JS/orbit.js
+++ Stellar/JS/orbit.js
@@ -1,3 +1,39 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var hero = document.getElementById("hero");
 var image = document.getElementById("hero_image");
 var title = document.getElementById("hero_title");
@@ -5,63 +41,47 @@ var content = document.getElementById("hero_content");
 var left_arrow = document.getElementById("left_arrow");
 var right_arrow = document.getElementById("right_arrow");
 var navbar = document.getElementById("navbar");
-var planets = {
-    sun: {
-        name: "The Sun",
-        about: "A massive ball of hot, glowing gas at the center of our solar system, providing light and energy for all planets.",
-        image: "./IMG/0_sun.svg",
-        navID: "nav_sun",
-    },
-    mercury: {
-        name: "Mercury",
-        about: "Mercury is the closest planet to the Sun, with extreme temperatures ranging from scorching to freezing.",
-        image: "./IMG/1_mercury.svg",
-        navID: "nav_mercury",
-    },
-    venus: {
-        name: "Venus",
-        about: "Venus, the second planet from the Sun, is a scorching world with a thick atmosphere, acidic clouds, and a runaway greenhouse effect.",
-        image: "./IMG/2_venus.svg",
-        navID: "nav_venus",
-    },
-    earth: {
-        name: "Earth",
-        about: "Our home planet, Earth is the only known celestial body to support life, featuring diverse ecosystems and a variety of climates.",
-        image: "./IMG/3_earth.svg",
-        navID: "nav_earth",
-    },
-    mars: {
-        name: "Mars",
-        about: "Known as the Red Planet, Mars has a thin atmosphere and is home to the tallest volcano and the deepest canyon in our solar system.",
-        image: "./IMG/4_mars.svg",
-        navID: "nav_mars",
-    },
-    jupiter: {
-        name: "Jupiter",
-        about: "The largest planet, Jupiter has a powerful magnetic field, numerous moons, and a distinctive banded appearance due to swirling clouds.",
-        image: "./IMG/5_jupiter.svg",
-        navID: "nav_jupiter",
-    },
-    saturn: {
-        name: "Saturn",
-        about: "Famous for its stunning ring system, Saturn is a gas giant with unique features like hexagonal cloud patterns at its north pole.",
-        image: "./IMG/6_saturn.svg",
-        navID: "nav_saturn",
-    },
-    uranus: {
-        name: "Uranus",
-        about: "Uranus is an ice giant with a tilted rotation axis, causing it to rotate on its side. It has a faint ring system and numerous moons.",
-        image: "./IMG/7_uranus.svg",
-        navID: "nav_uranus",
-    },
-    neptune: {
-        name: "Neptune",
-        about: "Neptune, the farthest planet from the Sun, is a vibrant blue gas giant with strong winds, dark storms, and a mysterious Great Dark Spot.",
-        image: "./IMG/8_neptune.svg",
-        navID: "nav_neptune",
-    },
-};
+// import data from './JS/DATA/data.json' and store it in the const planets
+var planets = [];
+// fetch('./JS/DATA/data.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     planets = data;
+//     // Use the planets data here or in subsequent code
+//     console.log(planets);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+function fetchData() {
+    return __awaiter(this, void 0, void 0, function () {
+        var response, data, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 3, , 4]);
+                    return [4 /*yield*/, fetch("./JS/DATA/data.json")];
+                case 1:
+                    response = _a.sent();
+                    return [4 /*yield*/, response.json()];
+                case 2:
+                    data = _a.sent();
+                    planets = data;
+                    // Use the planets data here or in subsequent code
+                    console.log(planets);
+                    return [3 /*break*/, 4];
+                case 3:
+                    error_1 = _a.sent();
+                    console.error("Error:", error_1);
+                    return [3 /*break*/, 4];
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
+fetchData();
 function setPlanet(planet) {
+    console.log(planet);
     // Update Hero
     image.setAttribute("src", planet.image);
     title.innerHTML = planet.name;
@@ -130,7 +150,7 @@ window.addEventListener("keydown", function (event) {
 // each scrollable item is 500px wide
 navbar.addEventListener("scroll", function () {
     var current_scroll = navbar.scrollLeft;
-    var scroll_ended = (current_scroll % 500) == 0;
+    var scroll_ended = current_scroll % 500 == 0;
     if (scroll_ended) {
         var target_planet_id = current_scroll / 500;
         var target_planet = Object.keys(planets)[target_planet_id];
@@ -150,8 +170,16 @@ function quick_switch() {
         _loop_1(planet);
     }
     var debugDelay = window.setTimeout(function () {
-        setPlanet(planets.sun);
+        setPlanet(planets["sun"]);
     }, myDelay);
 }
-setPlanet(planets.sun);
+function start_loading() {
+    console.log("Logging : All planets");
+    console.log(planets);
+    console.log("Logging : The sun");
+    console.log(planets["sun"]);
+    setPlanet(planets["sun"]);
+    return true;
+}
+setTimeout(start_loading, 3000);
 //# sourceMappingURL=orbit.js.map
--- Stellar/JS/orbit.ts
+++ Stellar/JS/orbit.ts
@@ -9,64 +9,129 @@ let right_arrow = document.getElementById("right_arrow");
 
 let navbar = document.getElementById("navbar");
 
-let planets = {
-	sun: {
-		name: "The Sun",
-		about: "A massive ball of hot, glowing gas at the center of our solar system, providing light and energy for all planets.",
-		image: "./IMG/0_sun.svg",
-		navID: "nav_sun",
-	},
-	mercury: {
-		name: "Mercury",
-		about: "Mercury is the closest planet to the Sun, with extreme temperatures ranging from scorching to freezing.",
-		image: "./IMG/1_mercury.svg",
-		navID: "nav_mercury",
-	},
-	venus: {
-		name: "Venus",
-		about: "Venus, the second planet from the Sun, is a scorching world with a thick atmosphere, acidic clouds, and a runaway greenhouse effect.",
-		image: "./IMG/2_venus.svg",
-		navID: "nav_venus",
-	},
-	earth: {
-		name: "Earth",
-		about: "Our home planet, Earth is the only known celestial body to support life, featuring diverse ecosystems and a variety of climates.",
-		image: "./IMG/3_earth.svg",
-		navID: "nav_earth",
-	},
-	mars: {
-		name: "Mars",
-		about: "Known as the Red Planet, Mars has a thin atmosphere and is home to the tallest volcano and the deepest canyon in our solar system.",
-		image: "./IMG/4_mars.svg",
-		navID: "nav_mars",
-	},
-	jupiter: {
-		name: "Jupiter",
-		about: "The largest planet, Jupiter has a powerful magnetic field, numerous moons, and a distinctive banded appearance due to swirling clouds.",
-		image: "./IMG/5_jupiter.svg",
-		navID: "nav_jupiter",
-	},
-	saturn: {
-		name: "Saturn",
-		about: "Famous for its stunning ring system, Saturn is a gas giant with unique features like hexagonal cloud patterns at its north pole.",
-		image: "./IMG/6_saturn.svg",
-		navID: "nav_saturn",
-	},
-	uranus: {
-		name: "Uranus",
-		about: "Uranus is an ice giant with a tilted rotation axis, causing it to rotate on its side. It has a faint ring system and numerous moons.",
-		image: "./IMG/7_uranus.svg",
-		navID: "nav_uranus",
-	},
-	neptune: {
-		name: "Neptune",
-		about: "Neptune, the farthest planet from the Sun, is a vibrant blue gas giant with strong winds, dark storms, and a mysterious Great Dark Spot.",
-		image: "./IMG/8_neptune.svg",
-		navID: "nav_neptune",
-	},
-};
+// ! original data!
+//#region planet data
+// const planets = {
+// 	sun: {
+// 		name: "The Sun",
+// 		about: "A massive ball of hot, glowing gas at the center of our solar system, providing light and energy for all planets.",
+// 		image: "./IMG/0_sun.svg",
+// 		navID: "nav_sun",
+// 	},
+// 	mercury: {
+// 		name: "Mercury",
+// 		about: "Mercury is the closest planet to the Sun, with extreme temperatures ranging from scorching to freezing.",
+// 		image: "./IMG/1_mercury.svg",
+// 		navID: "nav_mercury",
+// 	},
+// 	venus: {
+// 		name: "Venus",
+// 		about: "Venus, the second planet from the Sun, is a scorching world with a thick atmosphere, acidic clouds, and a runaway greenhouse effect.",
+// 		image: "./IMG/2_venus.svg",
+// 		navID: "nav_venus",
+// 	},
+// 	earth: {
+// 		name: "Earth",
+// 		about: "Our home planet, Earth is the only known celestial body to support life, featuring diverse ecosystems and a variety of climates.",
+// 		image: "./IMG/3_earth.svg",
+// 		navID: "nav_earth",
+// 	},
+// 	mars: {
+// 		name: "Mars",
+// 		about: "Known as the Red Planet, Mars has a thin atmosphere and is home to the tallest volcano and the deepest canyon in our solar system.",
+// 		image: "./IMG/4_mars.svg",
+// 		navID: "nav_mars",
+// 	},
+// 	jupiter: {
+// 		name: "Jupiter",
+// 		about: "The largest planet, Jupiter has a powerful magnetic field, numerous moons, and a distinctive banded appearance due to swirling clouds.",
+// 		image: "./IMG/5_jupiter.svg",
+// 		navID: "nav_jupiter",
+// 	},
+// 	saturn: {
+// 		name: "Saturn",
+// 		about: "Famous for its stunning ring system, Saturn is a gas giant with unique features like hexagonal cloud patterns at its north pole.",
+// 		image: "./IMG/6_saturn.svg",
+// 		navID: "nav_saturn",
+// 	},
+// 	uranus: {
+// 		name: "Uranus",
+// 		about: "Uranus is an ice giant with a tilted rotation axis, causing it to rotate on its side. It has a faint ring system and numerous moons.",
+// 		image: "./IMG/7_uranus.svg",
+// 		navID: "nav_uranus",
+// 	},
+// 	neptune: {
+// 		name: "Neptune",
+// 		about: "Neptune, the farthest planet from the Sun, is a vibrant blue gas giant with strong winds, dark storms, and a mysterious Great Dark Spot.",
+// 		image: "./IMG/8_neptune.svg",
+// 		navID: "nav_neptune",
+// 	},
+// };
+//#endregion planet data
 
-function setPlanet(planet: { image: string; name: string; about: string; navID: string }) {
+interface Planet {
+	name: string;
+	about: string;
+	image: string;
+	navID: string;
+	radius: string;
+	mass: string;
+	gravity: string;
+	distanceFromSun: string;
+	orbitalPeriod: string;
+	rotationPeriod: string;
+	moons?: string[];
+	atmosphere: string[];
+	surfaceTemperature: string;
+	explorationMissions: string[];
+	notableFeatures: string[];
+	surfaceComposition: string;
+	magneticField: string;
+	daytimeTemperature: string;
+	nighttimeTemperature: string;
+	geologicalHistory: string;
+	atmosphericPressure: string;
+	lunarPhases: string;
+	orbitalFeatures: string;
+	solarDay: string;
+	solarWindEffects: string;
+	uniqueFeatures?: string;
+	uniqueChallenges?: string;
+	landingSites?: string[];
+	potentialForLife?: string;
+	historicalSignificance?: string;
+}
+
+// import data from './JS/DATA/data.json' and store it in the const planets
+let planets: Planet[] = [];
+
+// fetch('./JS/DATA/data.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     planets = data;
+//     // Use the planets data here or in subsequent code
+//     console.log(planets);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+
+async function fetchData() {
+	try {
+		const response = await fetch("./JS/DATA/data.json");
+		const data = await response.json();
+		planets = data;
+		// Use the planets data here or in subsequent code
+		console.log(planets);
+	} catch (error) {
+		console.error("Error:", error);
+	}
+}
+
+fetchData();
+
+function setPlanet(planet: Planet) {
+	console.log(planet);
 	// Update Hero
 	image.setAttribute("src", planet.image);
 	title.innerHTML = planet.name;
@@ -136,17 +201,15 @@ window.addEventListener("keydown", function (event) {
 // Also detect when the navbar at the top is scrolled and makes a different planet name visible
 // each scrollable item is 500px wide
 navbar.addEventListener("scroll", function () {
-    const current_scroll = navbar.scrollLeft;
-    const scroll_ended = (current_scroll % 500) == 0;
-    if (scroll_ended) {
-        const target_planet_id = current_scroll / 500;
-        const target_planet = Object.keys(planets)[target_planet_id];
-        console.log(target_planet);
-        changePlanet(target_planet);
-    }
-
-
-})
+	const current_scroll = navbar.scrollLeft;
+	const scroll_ended = current_scroll % 500 == 0;
+	if (scroll_ended) {
+		const target_planet_id = current_scroll / 500;
+		const target_planet = Object.keys(planets)[target_planet_id];
+		console.log(target_planet);
+		changePlanet(target_planet);
+	}
+});
 
 function quick_switch() {
 	var myDelay = 1000;
@@ -159,8 +222,16 @@ function quick_switch() {
 	}
 
 	const debugDelay = window.setTimeout(function () {
-		setPlanet(planets.sun);
+		setPlanet(planets["sun"]);
 	}, myDelay);
 }
 
-setPlanet(planets.sun);
+function start_loading() {
+	console.log("Logging : All planets");
+	console.log(planets);
+	console.log("Logging : The sun");
+	console.log(planets["sun"]);
+	setPlanet(planets["sun"]);
+	return true;
+}
+setTimeout(start_loading,3000)
